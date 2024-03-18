d9679040821c6f2845561792e940aab74b59d19a
xiangn <xiangnrodriguez@gmail.com>
2024-03-18 19:38:10 -0400

    Implementado home menu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonButton, IonFooter, IonCardContent, IonLabel, IonInput, IonItem, IonIcon, IonText, IonCol, IonRow } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [ IonContent, IonCard, IonButton, IonCardContent ,
+     IonCardTitle, IonInput, IonItem, IonLabel, IonIcon,IonText,
+    IonCol,IonRow, IonCardContent],
 })
 export class HomePage {
   constructor() {}
