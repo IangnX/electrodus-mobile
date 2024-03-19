02f3d8949d06e6ae1380ae2f9bc060db71cd16a4
xiangn <xiangnrodriguez@gmail.com>
2024-03-19 07:15:17 -0400

    Registry page, estilos y validaciones

--- src/app/pages/registry/registry.page.ts
+++ src/app/pages/registry/registry.page.ts
@@ -1,20 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { IonCard } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-registry',
   templateUrl: './registry.page.html',
   styleUrls: ['./registry.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, ]
 })
-export class RegistryPage implements OnInit {
+export class RegistryPage {
 
-  constructor() { }
+  EMAIL_REGEXP = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder ,private router: Router) { }
+
+  form = this.formBuilder.group({
+    name: ['',[Validators.required, Validators.minLength(4)]],
+    lastName: ['',[Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.pattern(this.EMAIL_REGEXP)]],
+    password: ['', [Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(50),
+                   ]
+              ],
+    confirmPassword: ['', [Validators.required,
+                           Validators.minLength(8),
+                           Validators.maxLength(50)
+                          ]
+                      ],
+    dni: ['', [Validators.required,Validators.minLength(8)], Validators.pattern("^[0-9]+$"),]
+  });
+
+
+  goToLogin() {
+    this.router.navigate(['/login'])
   }
+
+  validFieldRequired(field :string): boolean{
+    return this.form.get(field)?.errors?.['required'] && this.form.get(field)?.touched
+  }
+
+  validFieldMinLength(field :string): boolean{
+    return (this.form.get(field)?.errors?.['minlength']?.actualLength != this.form.get(field)?.errors?.['minlength']?.requiredLength
+    && this.form.get(field)?.touched) || false;
+  }
+
+  validFieldPattern(field :string):boolean{
+    return this.form.get(field)?.errors?.['pattern'] && this.form.get(field)?.touched
+  }
+
+  validFieldPassword(): boolean{
+    return  this.form.controls.confirmPassword.touched && this.form.value.password != this.form.value.confirmPassword
+             && !this.validFieldMinLength('confirmPassword') && !this.validFieldRequired('confirmPassword')
+              && this.form.get('password')?.value !== ''
+  }
+
+  registry() {
+    console.log(this.form);
+    if(this.form.invalid){
+      this.form.markAllAsTouched();
+      return;
+    }
+  }
+
 
 }
