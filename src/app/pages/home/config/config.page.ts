325f328c91efe7b837e441dc17e0af954e087370
xiangn <xiangnrodriguez@gmail.com>
2024-03-19 20:19:16 -0400

    Implementando config page

--- src/app/pages/home/config/config.page.ts
+++ src/app/pages/home/config/config.page.ts
@@ -2,17 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { IonChip } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { exitOutline, chevronForwardOutline} from 'ionicons/icons';
+
+
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.page.html',
   styleUrls: ['./config.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonChip]
 })
 export class ConfigPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ exitOutline, chevronForwardOutline });
+  }
 
   ngOnInit() {
   }
