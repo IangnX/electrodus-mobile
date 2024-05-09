46030a5196109fb344c8b4043151d7f29aff165a
andres.gonzalez <xiangnrodriguez@gmail.com>
2024-05-08 21:42:52 -0400

    Agregado skeleton

--- src/app/pages/home/request/request.page.ts
+++ src/app/pages/home/request/request.page.ts
@@ -11,6 +11,7 @@ import { RequestService } from 'src/app/services/request.service';
 import { getRequestStatus, getRequestStatusColor } from 'src/app/utils/requestUtil';
 import { IonRefresherCustomEvent } from '@ionic/core';
 import { RequestPreviewPage } from 'src/app/interfaces/RequestPreviewPage';
+import { catchError, finalize } from 'rxjs';
 
 
 @Component({
@@ -29,6 +30,8 @@ export class RequestPage implements OnInit {
 
   requestPreviewList: RequestPreview[] = []
   private canDismissOverride = false;
+  isLoading = true;
+  error = null;
 
   constructor(private actionSheetCtrl: ActionSheetController,
     private requestService: RequestService) {
@@ -82,7 +85,25 @@ export class RequestPage implements OnInit {
   }
 
   getRequestList(event?: any,pull:boolean  = false): void {
-    this.requestService.getMyRequest(pull).subscribe((RequestPreviewPage:RequestPreviewPage)=> {
+
+    this.error = null;
+
+    // Only show loading indicator on initial load
+    if (!event) {
+      this.isLoading = true;
+    }
+
+    this.requestService.getMyRequest(pull)
+    .pipe(
+      finalize(() => {
+        this.isLoading = false;
+      }),
+      catchError((err: any) => {
+        this.error = err.error.status_message;
+        return [];
+      })
+    )
+    .subscribe((RequestPreviewPage:RequestPreviewPage)=> {
       console.log(RequestPreviewPage)
 
       this.requestPreviewList.push( ...RequestPreviewPage.content )
